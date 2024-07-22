bbdb98802047fb60e67d64d06061f0703450686d
hwooks96 <167948347+H-wooks@users.noreply.github.com>
2024-07-22 10:28:33 +0900

    Add files via upload

---
+++ M4_Web_APP/강의자료/ToDoList_06/models.py
@@ -0,0 +1,19 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy()
+
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    contents=db.Column(db.Text, nullable=False)
+    input_date=db.Column(db.Date, nullable=False, default=datetime.utcnow)
+    due_date=db.Column(db.Date, nullable=False)
+
+class User(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    username=db.Column(db.String(80), unique=True, nullable=False)
+    password=db.Column(db.String(200),nullable=False)
+    is_admin=db.Column(db.Boolean,default=False)
+    
